a3d8fb0cb315815dc55dd8610324867236b97346
Summit Digital <summitdigitalceo@gmail.com>
2026-03-24 12:36:54 -0400

    Fix: set NODE_TLS_REJECT_UNAUTHORIZED=0 for site scanning

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+// Allow scanning sites with self-signed or incomplete certificate chains
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
 const express = require('express');
 const path = require('path');
 const { execSync, spawn } = require('child_process');
