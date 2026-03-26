84ed051cee27e4a29e7ee38df298ad98d74581c1
Summit Digital <summitdigitalceo@gmail.com>
2026-03-26 02:50:32 -0400

    Blog post #8: Website Security Checklist for Small Business - 12-point checklist, SEO keyword target

--- server.js
+++ server.js
@@ -670,6 +670,9 @@ app.get('/blog/ecommerce-website-security-checklist', (req, res) => {
 app.get('/blog/why-your-restaurant-needs-a-fast-website', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'blog', 'why-your-restaurant-needs-a-fast-website.html'));
 });
+app.get('/blog/website-security-checklist-small-business', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'blog', 'website-security-checklist-small-business.html'));
+});
 app.get('/blog/free-website-audit-tool-small-business', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'blog', 'free-website-audit-tool-small-business.html'));
 });
